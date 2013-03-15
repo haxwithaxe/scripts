619b0d9b7aedbed12388b7d29f41de3c5cacccbf
haxwithaxe <me@haxwithaxe.net>
2013-03-15 10:09:34 -0400

    adding new scripts; gkrellm-immortal keeps gkrellm running after crashes, tilda-immortal restarts tilda after the last tab is closed, synclient-setup.sh sets the synaptic touchpad settings the way i like when i don't feel like editing xorg.conf

---
+++ synclient-setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+synclient \
+    VertEdgeScroll=1 \
+    HorizEdgeScroll=1 \
+    CornerCoasting=0 \
+    VertTwoFingerScroll=0 \
+    HorizTwoFingerScroll=0 \
+    TapButton1=0 \
+    TapButton2=0 \
+    TapButton3=0 \
+	PalmDetect=1 \
+    TapAndDragGesture=0

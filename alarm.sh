b75eece5a327ada2fe983adbffd817650095d037
haxwithaxe <me@haxwithaxe.net>
2014-06-16 01:41:05 -0400

    adding shiny bits to alarm.sh. getopts, some tweaks to try to get X11 to play nice from cron

--- alarm.sh
+++ alarm.sh
@@ -1,13 +1,64 @@
 #!/bin/bash
 
-_sound=${HOME}/resources/sounds/REDALERT.wav
-_play=paplay
+# run this at the beginning of every x session
+# xhost +si:localuser:$(whoami)
+export DISPLAY=:0.0
 
-message=${1:-"ALARM"}
+TEMP=`getopt -o xgp:s:m: -- "$@"`
+
+eval set -- "$TEMP"
+
+while true ;do
+	case $1 in
+		-x)
+			_xmessage=xmessage
+			shift
+			;;
+		-g)
+			_xmessage=gxmessage
+			shift
+			;;
+		-p)
+			_play=$2
+			shift 2
+			inmsg=false
+			;;
+		-s)
+			_sound=$2
+			shift 2
+			inmsg=false
+			;;
+		-m)
+			inmsg=true
+			message=$2
+			shift 2
+			;;
+		-d)
+			export DISPLAY=$2
+			shift 2
+			;;
+		--)
+			shift
+			break
+			;;
+		*)
+			if $inmsg ;then
+				message="$message $1"
+			fi
+			shift
+			;;
+	esac
+done
+
+_xmessage=${_xmessage:-gxmessage}
+_sound=${_sound:-/usr/share/sounds/KDE-Sys-App-Error-Critical.ogg}
+_play=${_play:-paplay}
+
+message=${message:-"ALARM"}
 
 while true ;do $_play $_sound ;done &
 
 play_pid=$!
 
-gxmessage $message
+$_xmessage $message
 kill -9 $play_pid

c73bb71102f46f9ef5d7ad234c18a79b6fc5075c
haxwithaxe <me@haxwithaxe.net>
2015-02-03 10:34:18 -0500

    added argument handling to synergyc.sh

--- synergyc.sh
+++ synergyc.sh
@@ -3,10 +3,33 @@
 # die hard
 set -e
 
+usage(){
+	echo "$(basename $0) [-s <server name/ip>] [-h] [<arguments for synergyc>]"
+}
+synergy_args=''
+
+while getopts "s:h" opt; do
+  case $opt in
+    s)
+    	SYNERGYS_IP=$OPTARG
+    	;;
+    h)
+    	usage
+		exit 1
+    	;;
+	*)
+		synergy_args="${synergy_args} $OPTARG"
+		;;
+  esac
+done
+
 # where synergyc will look for a server
-SYNERGYS_IP=${SYNERGYS_IP:-yoshi} #yoshi == server
+SYNERGYS_IP=${SYNERGYS_IP:-lenny} #yoshi == server
+
+# kill remaining instances of synergyc so this doesn't conflict
+killall synergyc || true
 
 # force synergyc to use :0.0 rather than the lack of X display in an 
 #  ssh session or the forwarded display
 # also pass any extra args and then the server ip
-synergyc --display :0.0 $@ $SYNERGYS_IP
+synergyc --display :0.0 $synergy_args $SYNERGYS_IP

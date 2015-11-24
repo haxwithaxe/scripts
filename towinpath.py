f9d167b5549e9e6dc7042f4fa92f8b5632d0f1d3
Chris Koepke <cmkoepke@getwellnetwork.com>
2015-11-23 20:55:42 -0500

    cleaned up and using the right tools

--- towinpath.py
+++ towinpath.py
@@ -1,15 +1,47 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
-import sys,re
+import os
+import ntpath
 
-drive = '\\'
-drivetypes = {'net':{'start':'\\\\','end':'\\'}}
+NET_TYPE = 'net'
+DISK_TYPE = 'disk'
 
-path = sys.argv[1]
-if len(sys.argv) > 2:
-	drive = sys.argv[2]
-	if drive.split(':')[0] in drivetypes:
-		dtype,drive = drive.split(':',1)
-		drive = drivetypes[dtype]['start']+drive+drivetypes[dtype]['end']
 
-print(drive+path.replace('\\','').replace('/','\\'))
+def _split_path(path):
+	path_list = []
+	path_tuple = (path, '')
+	while path_tuple[0]:
+		path_list.append(path_tuple[1])
+		path_tuple = os.path.split(path_tuple[0])
+	path_list.append(path_tuple[1])
+	path_list.reverse()
+	return path_list
+
+
+def to_windows_local(path, drive_letter=None):
+	if drive_letter:
+		drive_letter = drive_letter.strip(':\\')+':\\'
+	return _to_windows_path(drive_letter, path)
+
+
+def to_windows_net(path, domain=None):
+	domain = '\\\\'+(domain or '')
+	return _to_windows_path(domain, path)
+
+
+def _to_windows_path(drive_letter, path):
+	return ntpath.join(drive_letter or '', *_split_path(path))
+
+
+if __name__ == '__main__':
+	import argparse
+	parser = argparse.ArgumentParser()
+	parser.add_argument('--net', default=False, action='store_true')
+	parser.add_argument('--domain', default='')
+	parser.add_argument('--drive', default='')
+	parser.add_argument('path')
+	args = parser.parse_args()
+	if args.domain or args.net:
+		print(to_windows_net(args.path, args.domain))
+	else:
+		print(to_windows_local(args.path, args.drive))

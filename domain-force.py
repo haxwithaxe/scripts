a88add8e665e0cc019d374081e634bacb06a5171
haxwithaxe <spem@haxwithaxe.net>
2016-02-13 19:41:08 -0500

    domain-force.py actually runs now

--- domain-force.py
+++ domain-force.py
@@ -1,36 +1,74 @@
+
+import string
 import subprocess
 
-domain_chars = [chr(x) for x in [45] + range(48,57) + range(65,90)]
+domain_chars = string.ascii_lowercase+string.digits+'_-'
 
 tlds = ['com','co','xxx','asia','biz','mobi','tv','tel','org','net','info','us','ca','cc','bz','me','ch','de','hn']
 tlds_short = ['co','tv','us','ca','cc','bz','me','ch','de','hn']
 
 class Domain:
+
     def __init__(self, domain, tld):
         self.domain = domain
         self.tld = tld
 
-    def free(self):
-        output = subprocess.check_output(['whois', '%(domain)s.%(tld)s' % {'domain':self.domain,'tld':self.tld}], shell=True)
-        if output and 'Not found: ' in output:
-            return True
-        return False
+    def __eq__(self, other):
+        return self.domain == other.domain and self.tld == other.tld
 
-def for_tlds():
-    for tld in TLDS:
-        print('TLD: %s' % tld)
-	for_domains()
 
-def get_domain(state={'domain':[],'depth':0,'max':0}):
-    for pos in range(len(domain_chars)):
-        state['domain'].append(domain_chars[pos])
-        state['depth'] += 1
-        if state.get('depth') == state.get('max'):
-            d = state.get('domain')
-            d.append(domain_chars[pos])
-            dom = Domain(join(state.get('domain').append()), tld)
-            if dom.free():
-                print(dom.domain)
-        else:
-            get_domain(state)
+class TLD:
 
+    def __init__(self, value, domain_length=0):
+        self.value = value
+        self.domain_length = domain_length
+        self.domains = []
+
+    def __contains__(self, domain):
+        return domain in self.domains
+
+    def free(self, domain):
+        print('whois %(domain)s.%(tld)s' % {'domain':domain,'tld':self.value})
+        whois = subprocess.check_output('whois %(domain)s.%(tld)s' % {'domain':domain,'tld':self.value}, shell=True)
+        if whois and 'Not found: ' in whois.decode():
+            return Domain(domain, self.value)
+        return whois
+
+
+class Run:
+
+    def __init__(self, tlds, max_length):
+        self.tlds = tuple(TLD(tld) for tld in tlds)
+        self.max_length = max_length
+        self.domains = []
+        self.domains_gen = self.get_domains('', 0)
+        self._current_tld = None
+    
+    def get_domains(self, parent, depth):
+        if depth < self.max_length:
+            for char in domain_chars:
+                if depth == 0 and char in ('_', '-'):
+                    continue
+                yield parent+char
+                for child in self.get_domains(parent+char, depth+1):
+                    yield child
+
+    def found_free(self, domain):
+        if isinstance(domain, Domain):
+            if domain not in self.domains:
+                self.domains.append(domain)
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        domain = self.domains_gen.__next__()
+        for tld in self.tlds:
+            fulldomain = tld.free(domain)
+            self.found_free(fulldomain)
+            if isinstance(fulldomain, Domain):
+                return fulldomain
+
+if __name__ == '__main__':
+    r = Run(['us'], 1)
+    print([d for d in r])

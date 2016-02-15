f72b79f783de075f751a1d742ef0c96546512192
haxwithaxe <spem@haxwithaxe.net>
2016-02-14 21:44:52 -0500

    style cleanup

--- csv_filter.py
+++ csv_filter.py
@@ -2,7 +2,9 @@
 import argparse
 import sys
 
+
 FILTERS = lambda: {"gt":gt_filter, "lt":gt_filter, "eq":eq_filter, "ne":ne_filter, "null":null_filter}
+
 
 def run_filter(selected_filter):
     for line in [x for x in sys.stdin.read().strip().split('\n') if x]:
@@ -11,6 +13,7 @@ def run_filter(selected_filter):
             continue
         if selected_filter(items):
             print(line)
+
 
 def gt_filter(index, min_value, default):
     default = int(default)
@@ -21,6 +24,7 @@ def gt_filter(index, min_value, default):
         return False
     return gt
 
+
 def lt_filter(index, max_value, default):
     default = int(default)
     max_value = int(max_value)
@@ -29,12 +33,14 @@ def lt_filter(index, max_value, default):
             return True
     return lt
 
+
 def eq_filter(index, true_value, default):
     def eq(items):
         item = items[index] or default
         if true_value > item:
             return True
     return eq
+
 
 def ne_filter(index, true_value, default):
     def ne(items):
@@ -43,13 +49,16 @@ def ne_filter(index, true_value, default):
             return True
     return ne
 
+
 def null_filter(*args, **kwargs):
     return lambda x: True
+
 
 def main(args):
     filter_gen = FILTERS().get(args.filter_name, null_filter)
     filter_func = filter_gen(args.index, args.match_value, args.default_value)
     run_filter(filter_func)
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()

86ee6829524edff2c3bd15da01ae75ff0ba055f0
haxwithaxe <spem@haxwithaxe.net>
2016-02-14 21:44:23 -0500

    style cleanup

--- csv2html.py
+++ csv2html.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python2
-''' Very simplistic csv to html table converter '''
+""" Very simplistic csv to html table converter """
 
 
 __authors__ = ['haxwithaxe <me@haxwithaxe.net>']
-
 __license__ = 'GPLv3'
 
+
 import csv
-#FIXME: use xml library you twit
 
 
 def attrib_to_string(**attribs):
@@ -15,24 +14,30 @@ def attrib_to_string(**attribs):
         return ''
     return ' '.join(['%s="%s"' % (key,value) for key,value in attribs])
 
+
 def xml_elem(*strings, **attribs):
     return '<%s %s>%s</%s>' % (elem, attrib_to_string(**attribs) , ''.join(strings), elem)
+
 
 def td(*strings, **attribs):
     attribs['elem'] = 'td'
     return xml_elem(*strings, **attribs)
 
+
 def th(*strings, **attribs):
     attribs['elem'] = 'th'
     return xml_elem(*strings, **attribs)
+
 
 def tr(*strings, **attribs):
     attribs['elem'] = 'tr'
     return xml_elem(*strings, **attribs)
 
+
 def table(*string, **attribs):
     attribs['elem'] = 'table'
     return xml_elem(*string, **attribs)
+
 
 def load_csv(filename):
     with open(filename, 'rb') as csvfile:
@@ -40,11 +45,13 @@ def load_csv(filename):
         csvfile.seek(0)
         reader = csv.reader(csvfile, dialect)
 
+
 def make_header(reader):
     headers = []
     for f in reader.fieldnames:
         headers.append(th(f))
     return tr(*headers)
+
 
 def make_contents(reader):
     body = []
@@ -58,11 +65,13 @@ def make_contents(reader):
             body.append(tr(row))
     return body
 
+
 def make_table(reader):
     header = make_header(reader)
     body = make_contents(reader)
     header += body
     return table(header*)
+
 
 def make_page(reader):
     t = make_table(reader)

2aaa5be836fc26a7c0f46da98636b9229c7d8883
haxwithaxe <spam@haxwithaxe.net>
2016-02-22 13:03:49 -0500

    added xrandr_setup.sh and tab_complete.py

--- tab_complete.py
+++ tab_complete.py
@@ -1,12 +1,26 @@
 import os
 import readline
 import shlex
+<<<<<<< 55a23e4d0277ec2f24ad8098cdefb347290511fb
+=======
+import sys
+
+
+def stderr(*strings):
+    print(*strings, file=sys.stdout)
+>>>>>>> added xrandr_setup.sh and tab_complete.py
 
 
 def prep_readline( completer ):
     readline.set_completer_delims( '\n\t;' )
     readline.parse_and_bind( 'tab: complete' )
+<<<<<<< 55a23e4d0277ec2f24ad8098cdefb347290511fb
     readline.set_completer( completer )
+=======
+    readline.set_completer( completer.complete )
+    readline.set_completion_display_matches_hook(completer.display_matches)
+
+>>>>>>> added xrandr_setup.sh and tab_complete.py
 
 def _list_dir( directory ):
     files = []
@@ -17,6 +31,10 @@ def _list_dir( directory ):
         files.append( filename )
     return files
 
+<<<<<<< 55a23e4d0277ec2f24ad8098cdefb347290511fb
+=======
+
+>>>>>>> added xrandr_setup.sh and tab_complete.py
 def norm_join_path(dirname, filename ):
     path = os.path.join( dirname, filename )
     return os.path.normpath( path )
@@ -28,7 +46,11 @@ def expand_path( path ):
     return path
 
 
+<<<<<<< 55a23e4d0277ec2f24ad8098cdefb347290511fb
 def _complete_path( path ):
+=======
+def _complete_path( path, state = None ):
+>>>>>>> added xrandr_setup.sh and tab_complete.py
     if not path:
         return _list_dir('.')
     path = expand_path( path )
@@ -45,6 +67,7 @@ def _complete_path( path ):
 
 class Completer:
 
+<<<<<<< 55a23e4d0277ec2f24ad8098cdefb347290511fb
     def __init__( self, commands ):
         self.commands = commands
         self._state = self._text = self._line = None
@@ -67,6 +90,66 @@ class Completer:
         guesses.append( )
         return guesses
 
+=======
+    def __init__( self, commands, subcompleters = None ):
+        self.commands = commands
+        self.subcompleters = subcompleters
+        self._state = self._text = self._line = None
+
+    def complete(self, text, state):
+        return self._get_guesses(text, state)[state]
+
+    def _get_guesses( self, text, state ):
+        args = shlex.split( text )
+        if text:
+            args = shlex.split( text )
+            if args:
+                if self._is_command( args[0] ):
+                    stderr('command')
+                    return self._complete_command( args, state )
+                else:
+                    stderr('fallback')
+                    self._fallback(args, state)
+        cmds = [ x for x in self.commands.keys() ]
+        stderr(cmds)
+        return cmds
+        
+    def _is_command(self, first_string):
+        return [ x for x in self.commands.keys() if x.startswith( first_string ) or x == first_string ]
+
+    def _complete_command( self, args, state ):
+        command = args.pop().strip()
+        if command.strip() in self.commands:
+            return self.commands[command.strip()]( args )
+        guesses = [ guess + ' ' for guess in self.commands if guess.startswith( command ) ]
+        return guesses
+
+    def _fallback(self, args, state):
+        for completer in self.subcompleters:
+            yield completer(args, state)
+
+    def display_matches(self, substitution, matches, longest_match_length):
+        line_buffer = readline.get_line_buffer()
+        columns = os.environ.get('COLUMNS', 80)
+
+        print()
+
+        guess_template = '{:<' + str(int(max(map(len, matches)) * 1.2)) + '}'
+
+        guesses = ''
+        for match in matches:
+            match = guess_template.format(match[len(substitution):])
+            if len(guesses + match) > columns:
+                print(guesses)
+                guesses = ''
+            guesses += match
+
+        if guesses:
+            print(guesses)
+
+        print('> ', line_buffer, end='', flush=True)
+
+>>>>>>> added xrandr_setup.sh and tab_complete.py
 
 def _complete_hello( args ):
     if not args or not [ x for x in args if x ]:
@@ -77,6 +160,11 @@ def _complete_hello( args ):
 
 
 if __name__ == '__main__':
+<<<<<<< 55a23e4d0277ec2f24ad8098cdefb347290511fb
     comp = Completer( {'hello': _complete_hello } )
     prep_readline( comp.complete )
+=======
+    comp = Completer( {'hello': _complete_hello }, (_complete_path,) )
+    prep_readline( comp )
+>>>>>>> added xrandr_setup.sh and tab_complete.py
     input( 'hello tabcomplete>>> ' )

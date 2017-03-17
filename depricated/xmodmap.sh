0a237c17827bd346346a4915bac3d0b99c123d79
haxwithaxe <spam@haxwithaxe.net>
2017-03-17 10:45:13 -0400

    i can has spelling

--- depricated/xmodmap.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Caps lock = Mod4 (used by xmonad)
-xmodmap -e "clear lock" # drop Caps_Lock
-xmodmap -e "remove mod4 = Hyper_L" # remove redundant Hyper_L references
-xmodmap -e "keycode 66 = Hyper_L" # set Caps_Lock key to read as Hyper_L
-xmodmap -e "add mod3 = Hyper_L" # set  mod3 to be Hyper_L
-xmodmap -pm
